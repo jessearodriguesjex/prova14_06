4586c5700b567637fc9cf7dd148179b75818c172
jessearodriguesjex <jessearodrigues@gmail.com>
2024-06-14 20:06:52 -0300

    Add files via upload

--- Prova14_06.py
+++ Prova14_06.py
@@ -1,29 +1,58 @@
 
-import pandas as pd  
+#importando as bibliotecas
+import pandas as pd
 import streamlit as st
-
+import ipeadatapy as ip
+import matplotlib.pyplot as plt
 
 # título na barra do navegador
 st.set_page_config(
-page_title="Prova 14_06 - Exercício Projetos",
-  
+    page_title="Prova 14_06",
 )
 
-st.header("Dados do Projeto")
-arquivo = "https://raw.githubusercontent.com/jessearodriguesjex/prova14_06/main/projetos-1.csv" 
-df = pd.read_csv(arquivo, sep=';') 
-st.dataframe(dfe)
-
-
-st.write("Adicionando a ultima linha")
-
-df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
-dfe = pd.concat([df, df1])
-st.write(dfe)
 
 st.code(code, language='python')
 
+df = pd.read_csv("https://raw.githubusercontent.com/jessearodriguesjex/prova14_06/main/projetos-1.csv", sep=";") 
+st.dataframe(df)
+
+st.write("Uso do `st.experimental_data_editor()` para edição do dataframe na tela")
+
+code = '''edited_df = st.experimental_data_editor(df, num_rows="dynamic")'''
+st.code(code, language='python')
+
+edited_df = st.experimental_data_editor(df, num_rows="dynamic")
+
+"---"  
+
+st.write("Usando `st.checkbox()` para deixar o leitor escolher se vai mostrar a tabela ou não")
+
+code = '''
+if st.checkbox('Mostrar dataframe'):
+    # usando o streamlit para apresentar como df dinâmico e formatação adicional (max)
+    st.dataframe(df.style.highlight_max(axis=0)) 
+    '''
+st.code(code, language='python')
+
+if st.checkbox('Mostrar dataframe'):
+    # usando o streamlit para apresentar como df dinâmico e formatação adicional (max)
+    st.dataframe(df.style.highlight_max(axis=0)) 
+
+"---"    
+
+st.write("Adicionando nova linha")
+
+st.code(code, language='python')
+
+df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
+df = pd.concat([df, df1])
+st.write(df)
+
+"---"  
+
 st.write("Apresentação da soma dos valores de cada projeto agrupado por ano")
+
+st.code(code, language='python')
 
 st.write(df.groupby('ano').sum())
 
@@ -39,14 +68,18 @@ st.pyplot(fig)
 
 "---"  
 
-# Geração do gráfico
-fig, ax = plt.subplots()
-df["Projeto1"].plot(kind='hist', ax=ax)
-# Exibição do gráfico no Streamlit
-#st.pyplot(fig)
+st.write("Criação do gráfico do tipo histograma com os dados do `Projeto 1` e `Projeto4`")
 
 # Geração do gráfico
 #fig, ax = plt.subplots()
+df["Projeto4"].plot(kind='hist', ax=ax)
+# Exibição do gráfico no Streamlit
+st.pyplot(fig)
+
+st.code(code, language='python')
+
+# Geração do gráfico
+fig, ax = plt.subplots()
 df["Projeto4"].plot(kind='hist', ax=ax)
 # Exibição do gráfico no Streamlit
 st.pyplot(fig)
